e4c7368e2fe51c50c1302853d24e299d144e4f46
EdwinBanda <casemirobanda@gmail.com>
2024-04-15 15:30:18 +0200

    fix: changed the way to get the components, from .name/.css/.id to xpath

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
 const {Builder, Browser, By, until} = require('selenium-webdriver');
 
-(async function voidTest() {
+(async function sendMessage() {
   let driver = await new Builder().forBrowser(Browser.EDGE).build();
 
   await driver.get('https://void.co.mz');
-
-  const nome = driver.findElement(By.name('form_fields[name]'))
-  const email = driver.findElement(By.name('form_fields[email]'))
-  const campoMensagem = driver.findElement(By.name('form_fields[message]'))
-  // const captcha = driver.findElement(By.className('recaptcha-checkbox-border'))
-  const botaoEnviar = driver.findElement(By.css('input[type="submit"]'))
-  const success = driver.findElement(By.css('.elementor-message elementor-message-success'))
-
+  console.log("Started!")
+  const nome = driver.findElement(By.xpath('//*[@id="form-field-name"]'))
+  const email = driver.findElement(By.xpath('//*[@id="form-field-email"]'))
+  const campoMensagem = driver.findElement(By.xpath('//*[@id="form-field-message"]'))
+  // const captcha = driver.findElement(By.xpath('//*[@id="recaptcha-anchor"]/div[1]'))
+  const botaoEnviar = driver.findElement(By.xpath('//*[@id="form-project"]/div/form/div/div[5]/button'))
+  // const success = driver.findElement(By.xpath('//*[@id="form-project"]/div/form/div[2]'))
   await nome.sendKeys("Edwin Banda")
   await email.sendKeys("casemirobanda@gmail.com")
   await campoMensagem.sendKeys("Chamo me Edwin Banda, e este foi o meu teste técnico na Void!")
   // await captcha.click()
   await botaoEnviar.click()
+
   
-  await driver.wait(until.elementLocated(success), 10000)
+  // await driver.wait(until.elementLocated(success), 10000)
+  setTimeout(()=>{
+    console.log("Closing...")
+  },10000)
   console.log("Done!")
   await driver.quit();
 })();
 
-//delcioluis
